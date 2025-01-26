a380a147098ff9458cca6df3bceb06714220a851
kidonaru <kidonaru@gmail.com>
2025-01-26 11:29:39 +0900

    v1.1.0.0

--- source/COM3D2.ModItemExplorer.Plugin/PluginInfo.cs
+++ source/COM3D2.ModItemExplorer.Plugin/PluginInfo.cs
@@ -7,7 +7,7 @@ namespace COM3D2.ModItemExplorer.Plugin
     {
         public const string PluginName = "ModItemExplorer";
         public const string PluginFullName = "COM3D2." + PluginName + ".Plugin";
-        public const string PluginVersion = "1.0.0.2";
+        public const string PluginVersion = "1.1.0.0";
         public const string WindowName = PluginName + " " + PluginVersion;
 
         public readonly static byte[] Icon = Convert.FromBase64String(
